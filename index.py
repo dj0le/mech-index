68b95538cb6468d6a7e90da7399fe3b1bde696c2
dj0le <gstar12@hotmail.com>
2024-04-15 11:24:01 +0200

    vercel deploy

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import uvicorn
 from os import getenv
 
-if __name__ == "__main__":
+if __name__ == "__index__":
     port = int(getenv("PORT", 8000))
     uvicorn.run("mech_app.api:app", host="0.0.0.0", port=port,reload=True)
